11de35888c6ca3e95683ced76c8c8462b6ab21b7
GuiMM <guilherme.macedomoreira@gmail.com>
2024-09-15 15:28:13 -0300

    wip formata o file de infra RDS Mysql

--- variables.tf
+++ variables.tf
@@ -1,80 +1,80 @@
 variable "allocated_storage" {
   description = "The amount of allocated storage."
-  type = number
-  default = 20
+  type        = number
+  default     = 20
 }
 variable "storage_type" {
   description = "type of the storage"
-  type = string
-  default = "io2"
+  type        = string
+  default     = "io2"
 }
 variable "engine" {
   description = "The database engine"
-  type = string
-  default = "mysql"
+  type        = string
+  default     = "mysql"
 }
 variable "engine_version" {
   description = "The engine version"
-  default = "8.0.32"
-  type = string
+  default     = "8.0.32"
+  type        = string
 }
 variable "instance_class" {
   description = "The RDS instance class"
-  default = "db.t3.micro"
-  type = string
+  default     = "db.t3.micro"
+  type        = string
 }
 variable "parameter_group_name" {
   description = "Name of the DB parameter group to associate"
-  default = "default.mysql8.0"
-  type = string
+  default     = "default.mysql8.0"
+  type        = string
 }
 variable "username" {
   description = "Username for the master DB user."
-  default = "databaseteste"
-  type = string
+  default     = "databaseteste"
+  type        = string
 }
 variable "password" {
   description = "password of the database"
-  default = "password"
-  type = string
+  default     = "password"
+  type        = string
 }
 variable "skip_final_snapshot" {
   description = "skip snapshot"
-  default = "true"
-  type = string
+  default     = "true"
+  type        = string
 }
 variable "multi_az" {
   description = "projeto multi_az"
-  default = "false"
-  type = string
+  default     = "false"
+  type        = string
 }
-variable "db_name"{
+variable "db_name" {
   description = "projeto multi_az"
-  default = "burguer51"
-  type = string
+  default     = "burguer51"
+  type        = string
 }
-variable "db_user_name"{
+variable "db_user_name" {
   description = "projeto multi_az"
-  default = "root"
-  type = string
+  default     = "root"
+  type        = string
 }
-variable "db_password_name"{
+variable "db_password_name" {
   description = "projeto multi_az"
-  default = "rootrootroot"
-  type = string
+  default     = "rootrootroot"
+  type        = string
 }
 variable "identifier" {
   description = "The name of the RDS instance"
-  default = "terraform-database-test"
-  type = string
+  default     = "terraform-database-test"
+  type        = string
 }
 variable "port" {
   description = "The port on which the DB accepts connections"
-  default = "3306"
-  type = number
+  default     = "3306"
+  type        = number
 }
 variable "name" {
   description = "The database name"
-  default = "Mysqldatabase"
-  type = string
+  default     = "Mysqldatabase"
+  type        = string
 }
